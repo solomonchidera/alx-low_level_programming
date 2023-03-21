52b02a6bd3665d17b9ea75e9b3c51e7f4473c075
2003solomon <solochidera10@gmail.com>
2023-03-21 15:17:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the english alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+        char c = 'a';
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+                _putchar(c + i);
+        }
+        _putchar(10);
+}
