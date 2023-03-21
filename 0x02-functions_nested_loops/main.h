f1b72bb89f15a90bc404224adddeb915cdd5c40e
2003solomon <solochidera10@gmail.com>
2023-03-21 15:00:28 -0700

    fixing my _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alpabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jasck_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
