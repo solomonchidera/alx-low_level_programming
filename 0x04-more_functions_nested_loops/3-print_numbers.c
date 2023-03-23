ed8e708fa9af6d5e91103f68e1e1d7838f83fcb0
2003solomon <solochidera10@gmail.com>
2023-03-23 05:56:51 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - a program that prints numbers, 0 - 9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10)
+}
