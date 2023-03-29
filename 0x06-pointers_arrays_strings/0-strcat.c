47550acf1ca3b84528e9ce79639f426a8adbd915
2003solomon <solochidera10@gmail.com>
2023-03-29 19:34:33 +0100

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i, j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
+	for (i = 0; dest[i] != '\0' i++)
+		;
+	for (j = 0; src[j] != '\0' j++)
+
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+
 	return (dest);
+
 }
