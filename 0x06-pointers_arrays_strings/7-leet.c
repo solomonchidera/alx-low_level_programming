8aa3fe4f6b2326c2c05bb3c45e746f46f059ef9d
2003solomon <solochidera10@gmail.com>
2023-03-29 20:33:00 +0100

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
- * Return: void
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-void reverse_array(int *a, int n)
+char *leet(char *n)
 {
-	int i;
-	int t;
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; i < n--; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+		}
 	}
+	return (n);
 }
