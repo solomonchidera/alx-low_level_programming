98afeccaaadeb7aeb0da406342f039e4b82ae8fe
2003solomon <solochidera10@gmail.com>
2023-04-05 08:33:30 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	
+
 		_putchar('\n');
 	else
 	{
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,4 +1,4 @@
-#include <unsitd.h>
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
