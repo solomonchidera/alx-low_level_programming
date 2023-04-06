d9ed392edb129be45603866c6feb3ec8932a2b0b
2003solomon <solochidera10@gmail.com>
2023-04-06 02:05:29 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,9 +11,12 @@ int _strlen_recursion(char *s)
 	int i;
 
 	i = 0;
+
 	if (*s == '\0')
+	{
 		return 0;
 
-	i = 1 + _strlen_recursion(s + 1);
+	i = 1 +  _strlen_recursion(s + 1);
 	return (i);
+	}
 }
