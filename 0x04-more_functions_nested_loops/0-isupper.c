d6ebb02a4001a910069f3302f619a8e733a47eb8
2003solomon <solochidera10@gmail.com>
2023-04-10 07:57:07 +0100

    1-memcpy.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *_isupper - a function that checks for uppercase character
- *@c: a variable that stores the letters
+ * @c: a variable that stores the letters
  * Return: Always 1 if c is uppercase and 0 otherwise.
  */
 int _isupper(int c)
