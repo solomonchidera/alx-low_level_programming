b2d9b174256fc9904864bb966f4e88fef1082559
2003solomon <solochidera10@gmail.com>
2023-04-11 21:06:11 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * create_array - create array of size and assign char c
  * @size: size of array
  * @c: char to assign
  * Description: create array of size and assign char c
  * Return: pointers to array, NULL if fail
- */
-
+ *
+ **/
 char *create_array(unsigned int size, char c)
 {
 	char *str;
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,4 +11,5 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 int _putchar(char c);
+
 #endif /* MAIN_H */
