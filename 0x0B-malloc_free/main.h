fc905580b9a058c651f057f2a820c4351c0818b1
2003solomon <solochidera10@gmail.com>
2023-04-11 19:35:25 +0100

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-
+int _putchar(char c);
 #endif /* MAIN_H */
