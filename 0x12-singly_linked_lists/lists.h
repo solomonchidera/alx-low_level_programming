79b86e8733fe57c161ec68c51ee8efd23d70606b
2003solomon <solochidera10@gmail.com>
2023-04-28 21:22:01 +0100

    Added _putchar.c file prototype to my header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,7 +17,7 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 void construct(void) __attribute__((constructor));
