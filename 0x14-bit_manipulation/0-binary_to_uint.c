d5ff506fbe5ad55f5597e028a4feffd76426c753
2003solomon <solochidera10@gmail.com>
2023-05-05 13:27:21 +0100

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,42 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns new length of string
- * @s: the string (const)
- * Return: length of string
- */
-
-int _strlen(const char *s)
-{
-	int lent = 0;
-
-	while (s[lent] != '\0')
-	{
-		lent++;
-	}
-
-	return (lent);
-}
-
-/**
- * power_of - exponents
- * @base: base
- * @exp: exponent
- * Return: result (int)
- */
-
-int power_of(int base, int exp)
-{
-	int i, num;
-
-	num = 1;
-	for (i = 0; i < exp; ++i)
-		num *= base;
-
-	return (num);
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: binary number
  * Return: unsigned int
@@ -44,18 +8,24 @@ int power_of(int base, int exp)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
-	int leng, i;
+	int i;
+	unsigned int result = 0;
 
-	sum = 0;
-	if (b == NULL)
-		return (sum);
-	leng = _strlen(b);
-	for (i = leng - 1; i >= 0; i--)
+	if(!b)
+		return(0);
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '1')
+		{
+			result = result << 1;
+			result = result + 1;
+		}
+		else if(b[i] == '0')
+			result = result << 1;
+		else
 			return (0);
-		sum += (b[i] - '0') * power_of(2, leng - i - 1);
 	}
-	return (sum);
+
+	return (result);
 }
