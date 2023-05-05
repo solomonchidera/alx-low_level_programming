9a582ca8696e5f6591c94dee6edcdfd62c15aa53
2003solomon <solochidera10@gmail.com>
2023-05-05 13:31:47 +0100

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,8 +11,8 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int result = 0;
 
-	if(!b)
-		return(0);
+	if (!b)
+		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 			result = result << 1;
 			result = result + 1;
 		}
-		else if(b[i] == '0')
+		else if (b[i] == '0')
 			result = result << 1;
 		else
 			return (0);
