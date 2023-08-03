61eaf94d3a73cf4beca935e9cc73ee92db85e3ea
SolomonChidera <solochidera10@gmail.com>
2023-08-03 01:10:39 +0100

    ...

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,14 +8,16 @@
 
 int _strlen_recursion(char *s)
 {
+	int length = 0;
+
 	if (*s == '\0')
 	{
 		return (1);
 	}
 	else
 	{
-		return ((1 + _strlen_recursion(s + 1));
+		return ((length + _strlen_recursion(s + 1));
 	}
 
-
+	return (0);
 }
