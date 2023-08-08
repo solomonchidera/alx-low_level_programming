f33841eff003a88808c67c8ba68408d1675fba30
Solomon.Chidera <116091273+SolomonChidera@users.noreply.github.com>
2023-08-08 23:20:18 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/_putchar.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include "main.h"
-
-int _putchar(char c)
-{
-	return(write(1, &c, 1));
-}
--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -std=gnu89 *.c && ar -rc liball.a *.o
