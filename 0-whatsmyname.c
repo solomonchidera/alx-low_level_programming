7e3d8a25a1e7bf17a0fbc0ac314f63572dfc0e35
Solomon.Chidera <116091273+SolomonChidera@users.noreply.github.com>
2023-09-02 00:23:08 +0100

    Update 0-whatsmyname.c

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,16 +1,1 @@
-#include "main.h"
 
-/**
- * main - Entry point.
- * @argc: argument count.
- * @argv: argument vector
- *
- * Return: 0 always
- */
-
-int main(int argc, char *argv[])
-{
-	printf("%s\n", argv[0]);
-
-	return (0);
-}
