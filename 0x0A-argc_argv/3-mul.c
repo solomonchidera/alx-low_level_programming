c5cece21fbbc8d0c95ec50df4a4098c8e26c60ef
SolomonChidera <solochidera10@gmail.com>
2023-09-02 01:00:40 +0100

    Multiplying arguments(int) that was supplied to the program during execution

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - starting point of execution
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 always.
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, sum;
+	sum = 0;
+
+	if (argc > 1)
+	{
+		for (count = 0; count < argc; count++)
+			sum += atoi(argv);
+
+		printf("%d\n", sum);
+	}
+	else
+		printf("Error\n");
+
+	return (0);
+}
