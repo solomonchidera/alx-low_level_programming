89c55288fe83145e585f4e80d7a78d48ead1e852
SolomonChidera <solochidera10@gmail.com>
2023-09-12 05:40:45 +0100

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * init_dog - Write a function that initialize a variable of type struct dog
+ * @d: The dog to be initialized.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
