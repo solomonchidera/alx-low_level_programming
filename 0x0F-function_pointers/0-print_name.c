c123092493909b90edfd58a357f6259ab5010891
SolomonChidera <solochidera10@gmail.com>
2023-09-13 19:01:38 +0100

    Write a function that prints a name[test case].

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - funtion that takes a funtion pointer as an argument
+ * @name: string to be printed
+ * @f: funtion pointer
+ *
+ * Return: void
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
