14c1d8c342cd6f189c2c100a9929885c86a42f8d
SolomonChidera <solochidera10@gmail.com>
2023-09-13 16:46:51 +0100

    My header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef _FUNC_POINT_
 #define _FUNC_POINT_
 
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
