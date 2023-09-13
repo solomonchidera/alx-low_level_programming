f8227358d75ec33b8096045010a258ada14d74d3
SolomonChidera <solochidera10@gmail.com>
2023-09-13 18:44:07 +0100

    Header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNC_POINT_
-#define _FUNC_POINT_
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -8,6 +8,6 @@ int int_index(int *array, int size, int (*cmp)(int));
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stddef>
+#include <stddef.h>
 
 #endif
