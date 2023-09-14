b616a6043ac51ccd8d0c4c3f2a8f5a39a212450e
SolomonChidera <solochidera10@gmail.com>
2023-09-14 21:02:44 +0100

    Edit on Header file[added a prototype]

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
