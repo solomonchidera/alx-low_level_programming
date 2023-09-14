23f7ff953ff6bcbc9615e227dbb6a4a32fb47be0
Solomon.Chidera <116091273+SolomonChidera@users.noreply.github.com>
2023-09-14 20:53:43 +0100

    Update size.c

--- 0x00-hello_world/size.c
+++ 0x00-hello_world/size.c
@@ -17,5 +17,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	
 return (0);
 }
