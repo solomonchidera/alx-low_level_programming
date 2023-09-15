f82cc9f590f78edfda85c928b18cfd448d7de010
SolomonChidera <solochidera10@gmail.com>
2023-09-15 01:24:57 +0100

    Header file added a prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 int _putchar(char c);
+void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
