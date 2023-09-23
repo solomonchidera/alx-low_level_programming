18912f28c3a4b1d34797469343c6aa8d2edc8099
SolomonChidera <solochidera10@gmail.com>
2023-09-23 05:38:41 +0100

    Insert at the end of a linked list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,7 +19,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		len++;
 
 	new_node = malloc(sizeof(list_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
