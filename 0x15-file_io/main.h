bba87a151ece39f0210a27822dcfaa110b238987
SolomonChidera <solochidera10@gmail.com>
2023-10-02 21:38:20 +0100

    Initial commit on main.h[prototype for project]

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
