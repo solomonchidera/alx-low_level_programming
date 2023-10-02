87a13ba1aa9aedba2b50e78328aec3a6789b4a79
SolomonChidera <solochidera10@gmail.com>
2023-10-02 17:23:42 +0100

    Added Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,4 +5,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 int _putchar(int c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif
