1d39bf104580e91214467245421903577499ce9d
SolomonChidera <solochidera10@gmail.com>
2023-11-14 09:59:57 -0800

    Still crying for my laptop

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ELABDELLAOUI-HAJAR/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libshare.so 
+wget -P /tmp https://github.com/SolomonChidera/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libshare.so
 export LD_PRELOAD=/tmp/libshare.so
